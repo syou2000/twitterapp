46229e6d1f53eb360882be0dd90b51342271c3b5
syou2000 <syoutwilight@gmail.com>
2020-05-21 17:50:15 +0900

    rails db:migrate
    必要最低限のテーブルの作成

--- db/migrate/20200521081643_create_comments.rb
+++ db/migrate/20200521081643_create_comments.rb
@@ -1,7 +1,8 @@
-class CreateTweets < ActiveRecord::Migration[6.0]
+class CreateComments < ActiveRecord::Migration[6.0]
   def change
-    create_table :tweets do |t|
+    create_table :comments do |t|
       t.integer  :user_id,  null:false
+      t.integer  :tweet_id, null:false
       t.string   :text,     null:false
       t.timestamps
     end

d19d2ba11970a51270928028800c091104daffe1
syou2000 <syoutwilight@gmail.com>
2020-05-21 23:14:29 +0900

    toppage mark up
    トップページのマークアップ

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,9 @@
 class TweetsController < ApplicationController
   def index
-    @side = ["ホーム", "話題を検索", "通知", "メッセージ", "ブックマーク", "リスト", "プロフィール", "もっと見る"]
+    @tweet = Tweet.new
   end
+
+  # private
+  # def tweet_params
+  #   params.require(:tweet).permit(:text, :image, :user_id)
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,7 +3,7 @@ class Tweet < ApplicationRecord
   has_many  :favorites,  dependent: :destroy
   has_many  :favorited_users, through: :favorites,  source: :user
   has_many  :comments,  dependent: :destroy
-  has_one_attahed  :tweet_image,  dependent: :destroy
+  # has_one_attahed  :tweet_image,  dependent: :destroy
 
   attr_accessor :image
 end
